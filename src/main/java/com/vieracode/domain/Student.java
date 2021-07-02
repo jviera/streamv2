902cdc2f748c97912ab692f6f2aaf044434d1f96
José Viera <jviera.dev@gmail.com>
2021-07-02 00:23:24 -0600

    Create Student.java
    Student domain

---
+++ src/main/java/com/vieracode/domain/Student.java
@@ -0,0 +1,78 @@
+package com.vieracode.domain;
+
+import java.util.Objects;
+
+/**
+ * Student domain
+ */
+public class Student {
+    private int id;
+    private String name;
+    private String lastName;
+    private int level;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, String lastName, int level) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", level=" + level +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
