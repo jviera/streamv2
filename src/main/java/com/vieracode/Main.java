205166d4ec172a94723381f0ca8bceb7066a7831
José Viera <jviera.dev@gmail.com>
2021-07-02 01:08:38 -0600

    Update Main.java

--- src/main/java/com/vieracode/Main.java
+++ src/main/java/com/vieracode/Main.java
@@ -1,8 +1,19 @@
 package com.vieracode;
 
+import com.vieracode.domain.Student;
+import com.vieracode.service.StudentService;
+import com.vieracode.service.impl.StudentServiceImpl;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("Hello word!");
+        StudentService service = new StudentServiceImpl();
+        List<Student> students = service.getAll();
+
+        students.stream().filter(student -> student.getLevel()>2).map(Student::getName).forEach(System.out::println);
+
+
     }
 }
